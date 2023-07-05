301e3246a2e41b27b199bae7cc59ad707ab2cec1
Pratiksha <pratikshaanarase2272gmail.com>
2023-07-05 13:15:40 +0530

    upload all files

---
+++ src/main/java/com/pratiksha/ItemsApi/service/ItemService.java
@@ -0,0 +1,18 @@
+package com.pratiksha.ItemsApi.service;
+
+import com.pratiksha.ItemsApi.repository.entity.Item;
+
+import java.util.List;
+
+public interface ItemService
+{
+
+    Item save( Item item );
+
+    void delete( int itemId );
+
+    List<Item> all();
+
+    Item findById( int itemId );
+
+}
